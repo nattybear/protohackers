a18eb6850c77d577a519a58eb9e0a0ca80849c0c
vincent <vpark45@gmail.com>
2024-04-13 21:28:02 +0900

    Remove forever and move forkIO to outside of process

--- means-to-an-end/app/Main.hs
+++ means-to-an-end/app/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import           Control.Concurrent
-import           Control.Monad
 import           Data.Binary
 import qualified Data.ByteString.Lazy as L
 import           Data.Int
@@ -14,13 +13,14 @@ main = do
   sock <- socket AF_INET Stream 0
   bind sock (SockAddrInet 4000 0)
   listen sock 5
-  forever (session sock)
+  session sock
 
 session :: Socket -> IO ()
 session sock = do
   (conn, _) <- accept sock
   handle <- socketToHandle conn ReadWriteMode
-  process handle []
+  _ <- forkIO (process handle [])
+  session sock
 
 process :: Handle -> [(Int32,Int64)] -> IO ()
 process handle samples = do
@@ -29,13 +29,10 @@ process handle samples = do
   case message of
     Insert time price -> do
       let samples' = (time, fromIntegral price) : samples
-      _ <- forkIO (process handle samples')
-      return ()
+      process handle samples'
     Query mintime maxtime -> do
       let result = mean samples mintime maxtime
       L.hPut handle (encode result)
-      _ <- forkIO (process handle samples)
-      return ()
-    Undefined -> do
-      _ <- forkIO (process handle samples)
-      return ()
+      process handle samples
+    Undefined ->
+      process handle samples
